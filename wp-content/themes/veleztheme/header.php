5893b779bd8de58a373020f2fed02b221d162d20
craigavelez <craig.a.velez@gmail.com>
2015-11-09 17:31:51 -0500

    header link fix

--- wp-content/themes/veleztheme/header.php
+++ wp-content/themes/veleztheme/header.php
@@ -37,7 +37,7 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="head_wrap">
 				<div class="head_wrap_title">
-					<a class="velez" href="/#content" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<a class="velez" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 						<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 						<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 					</a>
