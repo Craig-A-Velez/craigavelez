f96ff000bf21f7e514a8898b3951402ba0b0b77d
craigavelez <craig.a.velez@gmail.com>
2015-11-12 12:50:05 -0500

    remove terrible 404 plugin and add 404 styles

--- wp-content/themes/veleztheme/404.php
+++ wp-content/themes/veleztheme/404.php
@@ -19,9 +19,9 @@ get_header(); ?>
 			<div class="page-wrapper">
 				<div class="page-content">
 					<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. How about we just head back to the homepage?', 'twentythirteen' ); ?></p>
+					<a href="/" alt="Home" class="resume">Go Home</a>
 
-					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</div><!-- .page-wrapper -->
 
