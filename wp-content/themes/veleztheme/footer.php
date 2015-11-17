885ee500d3f12009c6d08cfbac8fcaa4729bd0e7
craigavelez <craig.a.velez@gmail.com>
2015-11-17 10:38:29 -0500

    remove 404 plugin directory and move analytics to footer scripts

--- wp-content/themes/veleztheme/footer.php
+++ wp-content/themes/veleztheme/footer.php
@@ -18,7 +18,12 @@
 				<?php do_action( 'twentythirteen_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
-			<script>
+			
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -26,9 +31,5 @@
 		ga('create', 'UA-70270943-1', 'auto');
 		ga('send', 'pageview');
 	</script>
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
-
-	<?php wp_footer(); ?>
 </body>
 </html>
