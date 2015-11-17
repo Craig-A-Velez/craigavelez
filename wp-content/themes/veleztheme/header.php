1a9a1df91d5471f236d84642204f194429e84c8f
craigavelez <craig.a.velez@gmail.com>
2015-11-17 10:18:42 -0500

    remove 404 redirect plugin and add analytics script to header.php

--- wp-content/themes/veleztheme/header.php
+++ wp-content/themes/veleztheme/header.php
@@ -30,6 +30,14 @@
 	<?php wp_head(); ?>
 	<link href='https://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" id="twentythirteen-style-css" href="<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css" type="text/css" media="all">
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-70270943-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
